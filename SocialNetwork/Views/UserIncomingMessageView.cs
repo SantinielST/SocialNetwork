0cd34bd191be42d807c9a8fe54ae925533f44903
Name <email@example.ru>
2025-04-22 12:55:51 +0300

    Add messages views

---
+++ SocialNetwork/Views/UserIncomingMessageView.cs
@@ -0,0 +1,22 @@
+﻿using SocialNetwork.BLL.Models;
+
+namespace SocialNetwork.PLL.Views;
+
+internal class UserIncomingMessageView
+{
+    public void Show(IEnumerable<Message> incomingMessages)
+    {
+        Console.WriteLine("Входящие сообщения:");
+
+        if (incomingMessages.Count() == 0)
+        {
+            Console.WriteLine("Входящих сообщение нет!");
+            return;
+        }
+
+        incomingMessages.ToList().ForEach(incomingMessage =>
+        {
+            Console.WriteLine($"От кого: {incomingMessage.SenderEmail}. Текст сообщения: {incomingMessage.Content}");
+        });
+    }
+}
---
+++ SocialNetwork/Views/UserOutcomingMessageView.cs
@@ -0,0 +1,22 @@
+﻿using SocialNetwork.BLL.Models;
+
+namespace SocialNetwork.PLL.Views;
+
+internal class UserOutcomingMessageView
+{
+    public void Show(IEnumerable<Message> outcomingMessages)
+    {
+        Console.WriteLine("Исходящие сообщения");
+
+        if (outcomingMessages.Count() == 0)
+        {
+            Console.WriteLine("Исходящих сообщений нет!");
+            return;
+        }
+
+        outcomingMessages.ToList().ForEach(outcomingMessage =>
+        {
+            Console.WriteLine($"Кому: {outcomingMessage.RecipientEmail}. Текст сообщения: {outcomingMessage.Content}");
+        });
+    }
+}
